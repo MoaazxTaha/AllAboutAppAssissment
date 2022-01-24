e92847aa781e2dfa447023df99b86cc9c2d1501c
Moaaz Taha <eng.moaazxtaha@gmail.com>
2022-01-24 13:02:24 +0100

    Initial Commit

---
+++ AllAoboutAppsAssissment/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AllAoboutAppsAssissment
+//
+//  Created by Moaaz Ahmed Fawzy Taha on 24/01/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
